81bd4054ad4d0ab02c9707ee58a85c85bc3c2ce1
zwei0719 <14751141@me.mcu.edu.tw>
2026-04-22 11:53:51 +0800

    Delete week07 directory

--- week07/week07-3a.c
+++
@@ -1,8 +0,0 @@
-// week07-3a.cpp SOIT107_Base_014
-#include <stdio.h>
-int main()
-{
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    printf("%d", a * 50 + b * 5 + c);
-}
--- week07/week07-3b.cpp
+++
@@ -1,9 +0,0 @@
-// week07-3b.cpp SOIT107_Base_017
-#include <stdio.h>
-int main()
-{
-    int a, b;
-    scanf("%d%d", &a, &b);
-    int ans = a - b * 6;
-    printf("%d %d", ans / 6, ans % 6);
-}
